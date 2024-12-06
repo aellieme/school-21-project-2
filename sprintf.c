a75296fc6f881a8b9753bd60ef99b75c42ac60fd
Aliya <aliya11yakupova@gmail.com>
2024-12-06 20:07:44 +0300

    sprintf.c частично

--- sprintf.c
+++ sprintf.c
@@ -1,3 +1,34 @@
-#include "s21_string.h"
+#include <stdarg.h>
+#include <stdio.h>
 
-int s21_sprintf(char *str, const char *format, ...);
+void print_strings(char* bufer, const char* string, ...);
+
+int main() {
+    char where[100];
+    print_strings(where, "Sun != %s  %s   %s", "Sunshine", "Su3hine", "Sunshine");
+    printf("%s\n", where);
+    return 0;
+}
+
+void print_strings(char* bufer, const char* string, ...) {
+    va_list args;
+    va_start(args, string);
+
+    int i, j = 0;
+    while (string[j] != '\0') {
+        if (string[j] == '%') {
+            switch (string[j + 1]) {
+                case 's': {
+                    char* word = va_arg(args, char*);
+                    for (int j = 0; word[j] != '\0'; j++) {
+                        bufer[i++] = word[j];
+                    }
+                    j += 2;
+                    break;
+                }
+            }
+        }
+        bufer[i++] = string[j++];
+    }
+    va_end(args);
+}
