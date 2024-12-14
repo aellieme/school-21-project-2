8c50ad58dafb318d23a582b26e44dc7492294d00
Hawkgibr <marinasavina627@gmail.com>
2024-12-14 23:55:00 +0500

    to_lower.c new

--- to_lower.c
+++ to_lower.c
@@ -8,14 +8,13 @@
 
 // Функция для преобразования строки в верхний регистр
 void *s21_to_lower(const char *str) {
-    if (str == s21_NULL)
-        return s21_NULL;
-
+    char *result = s21_NULL;
+    if (str != s21_NULL){
     s21_size_t len = 0;  // Используем size_t для длины строки
     while (str[len] != '\0') len++;
 
-    char *result = (char *)malloc((len + 1) * sizeof(char));
-    if (result == s21_NULL) return NULL;
+    result = (char *)malloc((len + 1) * sizeof(char));
+    if (result != s21_NULL)
 
     for (s21_size_t i = 0; i < len; i++) {  // Используем size_t для индекса
         if (str[i] >= 'A' && str[i] <= 'Z') {   //является ли символ заглавной буквой
@@ -25,9 +24,11 @@ void *s21_to_lower(const char *str) {
         }
     }
     result[len] = '\0';
-
+    }
     return result;
-}
+    }
+
+
 
 int main() {
     const char *input = "HELLO!";
