50aa5e874db62620bc9eab760ebcb87f75634948
Hawkgibr <marinasavina627@gmail.com>
2024-12-15 16:47:11 +0500

    trim.c new

--- trim.c
+++ trim.c
@@ -19,24 +19,26 @@ s21_size_t s21_strlen(const char *string){
 
 // Функция для удаления начальных и конечных символов из строки
 void *s21_trim(const char *src, const char *trim_chars) {
-    if (src == s21_NULL || trim_chars == s21_NULL)
-        return s21_NULL;  
+        char *result = s21_NULL;
+
+    if (src != s21_NULL || trim_chars != s21_NULL){
 
     s21_size_t src_len = s21_strlen(src);  // Длина исходной строки
     s21_size_t trim_len = s21_strlen(trim_chars);  // Длина строки с символами для удаления
 
-    // Находим индекс первого символа, который не является одним из trim_chars
+    // Находим индекс первого символа
     s21_size_t start_index = 0;
     while (start_index < src_len) {
         int found = 0;
         for (s21_size_t i = 0; i < trim_len; i++) {
             if (src[start_index] == trim_chars[i]) {
                 found = 1;
-                break;
+                
             }
         }
         if (!found) break;
         start_index++;
+    
     }
 
     // Находим индекс последнего символа, который не является одним из trim_chars
@@ -46,7 +48,6 @@ void *s21_trim(const char *src, const char *trim_chars) {
         for (s21_size_t i = 0; i < trim_len; i++) {
             if (src[end_index] == trim_chars[i]) {
                 found = 1;
-                break;
             }
         }
         if (!found) break;
@@ -57,17 +58,18 @@ void *s21_trim(const char *src, const char *trim_chars) {
     s21_size_t result_len = end_index - start_index + 1;
 
     // Выделяем память для результирующей строки
-    char *result = (char *)malloc((result_len + 1) * sizeof(char));
-    if (result == s21_NULL) return NULL;  // Проверяем, успешно ли выделена память
+    result = (char *)malloc((result_len + 1) * sizeof(char));
+    if (result != s21_NULL) { 
 
     // Копируем символы из src в result
     for (s21_size_t i = 0; i < result_len; i++) {
         result[i] = src[start_index + i];
     }
 
-    result[result_len] = '\0';  // Добавляем завершающий нулевой символ
-
-    return result;  // Возвращаем указатель на новую строку
+    result[result_len] = '\0';
+    }
+    }
+    return result;
 }
 
 int main() {
@@ -80,7 +82,6 @@ int main() {
         printf("Before: \"%s\"\n", src);
         printf("After: \"%s\"\n", output);
 
-        // Освобождаем память, выделенную с помощью malloc
         free(output);
     } else {
         printf("Error\n");
