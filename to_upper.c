fa96d2a923595d4c6ca691535b52f4222221deb5
Hawkgibr <marinasavina627@gmail.com>
2024-12-18 23:30:29 +0500

    to_upper.c new

--- to_upper.c
+++ to_upper.c
@@ -6,15 +6,23 @@
 #define s21_NULL (void *)0
 #define s21_size_t unsigned long long
 
+s21_size_t s21_strlen(const char *str) {
+    s21_size_t len = 0;
+    if (str != s21_NULL) {
+        while (str[len] != '\0') {
+            len++;
+        }
+    }
+    return len;
+}
 
-// Функция для преобразования строки в верхний регистр
 void *s21_to_upper(const char *str) {
     char *result = s21_NULL;
     if (str != s21_NULL){
     s21_size_t len = 0;  // Используем size_t для длины строки
     while (str[len] != '\0') len++;
 
-    *result = (char *)malloc((len + 1) * sizeof(char));
+    result = (char *)malloc((len + 1) * sizeof(char));
     if (result != s21_NULL)
 
     for (s21_size_t i = 0; i < len; i++) {  // Используем size_t для индекса
@@ -25,10 +33,10 @@ void *s21_to_upper(const char *str) {
         }
     }
     result[len] = '\0';
-
+    }
     return result;
 }
-}
+
 
 int main() {
     const char *input = "Hello, World!";
