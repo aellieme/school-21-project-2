c4773a51a6cf9b6106bacd34ad9ad287d9dc277a
Aliya <aliya11yakupova@gmail.com>
2024-12-25 10:18:27 +0300

    Подправил поиск флагов, убрав повторяющийся код

--- s21_sprintf/s21_sprintf.c
+++ s21_sprintf/s21_sprintf.c
@@ -2,11 +2,9 @@
 
 int main() {
     char buffer[1028] = {0};
-
-    s21_sprintf(buffer, "Hello %d", -3);
+    s21_sprintf(buffer, "Hello %hd", 1951);
     printf("%s\n", buffer);
-
-    sprintf(buffer, "Hello %d", -3);
+    sprintf(buffer, "Hello %hd", 1951);
     printf("%s\n", buffer);
 
     return 0;
@@ -15,44 +13,112 @@ int main() {
 int s21_sprintf(char *str, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    int j = 0, i = 0, type = 0;
+    int j = 0, i = 0; // возможно можно убрать
 
     while (format[j] != '\0') {
         if (format[j] != '%') {
             str[i++] = format[j++];
         } else {
             j++;
-            if (format[j] == 'c') specifier_c(str, &i, args);
-            if (format[j] == 'd') specifier_d(str, &i, args, 0);
-           if (format[j] == 'u') specifier_u(str, &i, args);
-            // if (format[j] == 'f') specifier_f(str, &i, args);
-            if (format[j] == 's') specifier_s(str, &i, args);
-            if (format[j] =='l') specifier_l(str, format, &i, &j, args);
-            if (format[j] == '%') specifier_per(str, format, &i, &j);
+            flag_struct flags = {0};
+            parser_sign(format, &flags, &j);
+            parser_flags(format, &flags, &j);
+            // parser_accuracy(format, );
+            if (format[j] == '.') accuracy(str, format, &i, &j, args);
+            else if (format[j] == 'c') specifier_c(str, &i, args);
+            else if (format[j] == 'd') specifier_d(str, &i, args, flags);
+            else if (format[j] == 'u') specifier_u(str, &i, args, flags);
+//            else if (format[j] == 'f') specifier_f(str, &i, args);
+            else if (format[j] == 's') specifier_s(str, &i, args);
+            // else if (format[j] =='l') specifier_l(str, format, &i, &j, args);
+            else if (format[j] == '%') specifier_per(str, format, &i, &j);
             j++;
         }
     }
 
     va_end(args);
 
-    return 0;
+    return i;
 }
+
+void parser_flags(const char* format, flag_struct* flags, int* j) {
+    while (format[*j] == 'l' || format[*j] == 'h' || format[*j] == 'L') {
+        if (format[*j] == 'l') {
+            flags->flag_l = 1;
+        } else if (format[*j] == 'h') {
+            flags->flag_h = 1;
+        } else if (format[*j] == 'L') {
+            flags->flag_big_l = 1;
+        }
+        (*j)++;
+    }
+}
+
+void parser_sign(const char* format, flag_struct* flags, int* j) {
+    while (format[*j] == '-' || format[*j] == '+' || format[*j] == ' ' || format[*j] == '0' || format[*j] == '#') {
+        if (format[*j] == '-') {
+            flags->minus = 1;
+        } else if (format[*j] == '+') {
+            flags->plus = 1;
+        } else if (format[*j] == ' ') {
+            flags->space = 1;
+        } else if (format[*j] == '0') {
+            flags->zero = 1;
+        } else if (format[*j] == '#') {
+            flags->hash = 1;
+        }
+        (*j)++;
+    }
+}
+
+void accuracy(char *str, const char* format, int *i, int *j, va_list args) {
+   char number[100] = {0};
+   int k = 0;
+   int count = 0;
+    if (format[*j+1] == '*') {
+        int ac = va_arg(args, int);
+        if (format[*j+2] == 'd') {
+        long int d = va_arg(args, int);
+
+        while( d > 0) {
+            number[k++] = (d % 10) + '0';
+            d /= 10;
+            count++;
+        }
+        while (count < ac) {
+            number[k++] = (d % 10) + '0';
+            count++;
+        }
+        for (int z = k - 1; z >= 0; z--) {
+         str[(*i)++]  = number[z]; 
+               }
+            }
+    }
+    (*j) = (*j) + 2;
+}
+
+
 
 void specifier_c(char *str, int *i, va_list args) {
     char ch = (char)va_arg(args, int);
     str[(*i)++] = ch;
-    //    (*j)++;
 }
 
-void specifier_d(char *str, int *i, va_list args, int is_unsigned) {
+void specifier_d(char *str, int *i, va_list args, flag_struct flags) {
+    long int d = 0;
+    if (flags.flag_l) {
+        d = va_arg(args, long int);
+    } else if(flags.flag_h) {
+        d = (short)va_arg(args, int);
+    } else {
+        d = va_arg(args, int);
+    }
 
-    unsigned long d = is_unsigned ? va_arg(args, unsigned int) : va_arg(args, int);
     char number[100] = {0};
     int k = 0;
-
-    if (!is_unsigned && (int)d < 0) {
+    if (d < 0) {
         str[(*i)++] = '-';
-        d = -((int)d);
+        d = -(d);
     }
 
     if (d == 0) {
@@ -67,12 +133,13 @@ void specifier_d(char *str, int *i, va_list args, int is_unsigned) {
     for (int z = k - 1; z >= 0; z--) {
         str[(*i)++] = number[z];
     }
-    printf("%zu\n", sizeof(d));
 }
 
 
 // void specifier_f(char *str, int *i, va_list args) {
-//     double f = va_arg(args, double);
+//     double l = va_arg(args, double);
+//     double f = (round)(l * 1000000);
+//     f = f / 1000000;
 //     char number[100] = {0};
 //     int k = 0, count = 0;
 //     int d = (int)f;
@@ -100,8 +167,7 @@ void specifier_d(char *str, int *i, va_list args, int is_unsigned) {
 //             str[(*i)++] = number[z];
 //         }
 //         str[(*i)++] = '.';
-//         double h = (round)((f - (long long)f) * 1000000);
-//         h = h / 1000000;
+//         double h = f - (long long)f;
 
 //         while (count < 6) {
 //             h = h * 10;
@@ -119,18 +185,24 @@ void specifier_s(char *str, int *i, va_list args) {
     }
 }
 
-void specifier_u(char *str, int *i, va_list args) {
-    unsigned int u = va_arg(args, unsigned int);
+void specifier_u(char *str, int *i, va_list args, flag_struct flags) {
+    unsigned long int u = 0;
+    if (flags.flag_l) {
+        u = va_arg(args, unsigned int);
+    } else if (flags.flag_h) {
+        u = (short unsigned int)va_arg(args, unsigned int);
+    }
+
     char number[100] = {0};
     int k = 0;
-
+    if (u == 0) {
+        str[(*i)++] = '0';
+    }
     while (u > 0) {
         number[k] = (u % 10) + '0';
         k++;
         u = u / 10;
     }
-    // (*j)++;
-
     for (int z = k - 1; z >= 0; z--) {
         str[(*i)++] = number[z];
     }
@@ -155,15 +227,4 @@ void specifier_per(char *str, const char *format, int *i, int *j) {
         }
     }
     // Прописать иначе - NULL и остановка программы
-}
-
-void specifier_l(char *str, const char* format, int *i, int* j, va_list args) {
-    if (format[*j+1] == 'd') {
-        specifier_d(str, i, args, 1);
-        (*j)++;
-    } else  if (format[*j+1] == 'u') {
-//        specifier_u(str, i, args, 0);
-        (*j)++;
-    }
-    // if (format[j] == 'u') specifier_u(str, &i, args);
 }
